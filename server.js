c5bd0d336bee4296c78f1149c832a5a924af703f
xialiu1988 <liuxia198896@gmail.com>
2018-12-11 11:54:12 -0800

    updated

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express=require('express');
 const app=express();
-
+const cors=require('cors');
 const PORT=process.env.PORT||3000;
-
+const superagent=require('superagent');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
-
+require('dotenv').config();
 app.set('view engine','ejs');
 
 app.get('/hello',(req,res)=>{
@@ -17,13 +17,56 @@ app.get('/',(req,res)=>{
   res.render('../views/pages/error');
 });
 
-app.get('/',(req,res)=>{
-  res.render('../views/pages/searches/show');
-});
+// app.get('/',(req,res)=>{
+//   res.render('../views/pages/searches/show',{booklist:list});
+// });
 
-app.post('/search', (req, res) => {
-  console.log('my search body', req.body);
+// app.post('/search', (req, res) => {
+//   console.log('my search body', req.body);
+//   res.sendFile('../views/pages/searches/show',{booklist:list});
+// })
+
+app.post('/search',getsearch);
+ 
+function getsearch(req,res){
+
+let handler= req.body;
+
+Book.getBookinfo(handler)
+.then(res.render('/searches/show',{booklist:list}));
+
+
+}
+
+
+function Book(query,data){
+this.search_query=query;
+this.tile=data.volumeInfo.title;
+this.authors=data.volumeInfo.authors;
+this.publisher=data.volumeInfo.publisher;
+this.description=data.volumeInfo.description;
+
+}
+
+
+
+Book.getBookinfo=(query)=>{
+
+const _URL=`https://www.googleapis.com/books/v1/volumes?q=${query}`;
+return superagent.get(_URL)
+.then(data=>{
+
+let book=new Book(query,data.body.items);
+return book;
+
 })
+
+
+};
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`listening on ${PORT}`);
