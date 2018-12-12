ee9ced55a90c8849a1cb7d12071d59dcf23b6485
xialiu1988 <liuxia198896@gmail.com>
2018-12-11 16:10:37 -0800

    added author choice

--- server.js
+++ server.js
@@ -30,18 +30,23 @@ app.post('/search',getsearch);
 function getsearch(req,res){
   let arr=[];
   const titleURL=`https://www.googleapis.com/books/v1/volumes?q=intitle:${req.body.searchkey}`;
+  const authorURL=`https://www.googleapis.com/books/v1/volumes?q=inauthor:${req.body.searchkey}`;
+//  const url=` https://www.googleapis.com/books/v1/volumes?q=${req.body.searchkey}+${req.body.search}:${req.body.searchkey}`;
   console.log(req.body);
+
+
+  if(req.body.search==='title'){
 return superagent.get(titleURL)
 .then(data=>{
   
 data.body.items.forEach(book=>{
 
   let obj = {
+    image_url: book.volumeInfo.imageLinks.thumbnail,
     title: book.volumeInfo.title,
-    author: book.volumeInfo.authors ? book.volumeInfo.authors[0] : 'No Author',
-    description: book.volumeInfo.description,
     isbn: parseInt(book.volumeInfo.industryIdentifiers[0].identifier),
-    image_url: book.volumeInfo.imageLinks.thumbnail
+    author: book.volumeInfo.authors ? book.volumeInfo.authors[0] : 'No Author',
+    description: book.volumeInfo.description
   };
 
   arr.push(obj);
@@ -50,10 +55,34 @@ data.body.items.forEach(book=>{
 res.render('../views/pages/show',{data:arr});
 })
 
+  }
 
+  else{
+    return superagent.get(authorURL)
+    .then(data=>{
+      
+    data.body.items.forEach(book=>{
+    
+      let obj = {
+        image_url: book.volumeInfo.imageLinks.thumbnail,
+        title: book.volumeInfo.title,
+        isbn: parseInt(book.volumeInfo.industryIdentifiers[0].identifier),
+        author: book.volumeInfo.authors ? book.volumeInfo.authors[0] : 'No Author',
+        description: book.volumeInfo.description
+      };
+    
+      arr.push(obj);
+    
+    });
+    res.render('../views/pages/show',{data:arr});
+    })
+    
+
+
+
+  }
 
 }
-
 
 
 
